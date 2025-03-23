3775b71e7e95d19ca32c0f298d1e0155c6f2c786
Aqdas <123251973+Aqdashere@users.noreply.github.com>
2025-03-23 03:03:41 +0100

    New1

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class Tyamo extends StatelessWidget {
       builder: (context, orientation, screenType) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Tyamo App',
+          title: 'Tyamo',
           home: Homepage(),
         );
       },
