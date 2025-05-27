63ea5097b0c0cb9b8c462a17c061ce712df2b3bb
Aqdas <123251973+Aqdashere@users.noreply.github.com>
2025-05-27 14:26:42 +0200

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:tyamo/Views/Auth/Features/Deviceinfo/UserStatus/user_status.dart';
+import 'package:tyamo/Views/Auth/Features/Deviceinfo/device_info.dart';
 import 'package:tyamo/Views/Auth/login.dart';
 import 'package:tyamo/Views/Auth/register.dart';
 import 'package:tyamo/Views/Auth/forgot_password.dart';
+import 'package:tyamo/Views/Homepage/dashboard.dart';
 import 'package:tyamo/Views/Homepage/homepage.dart';
 import 'package:tyamo/Views/Invitation/accept_invite.dart';
 import 'package:tyamo/Views/Invitation/invite_friend.dart';
